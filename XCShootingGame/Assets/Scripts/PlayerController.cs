4c7d7dd7fd520d979b3afbe384077a16280aef46
ScottXileChen <53885585+ScottXileChen@users.noreply.github.com>
2019-12-19 19:46:27 -0800

    [XCShootingGame]
    Add player controller.
    Player could move ahead.
    
    Need to do:
    Finish movement.

---
+++ XCShootingGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+    [SerializeField]
+    private float _moveSpeed;
+    [SerializeField]
+    private float _jumpSpeed;
+    void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float jump = Input.GetAxis("Jump");
+        Vector3 displacement = new Vector3(horizontal * _moveSpeed * Time.deltaTime, jump * _jumpSpeed * Time.deltaTime, vertical * _moveSpeed * Time.deltaTime);
+        _rigidbody.MovePosition(transform.position + displacement);
+
+        _animator.SetFloat("WalkingSpeed", vertical);
+    }
+}
